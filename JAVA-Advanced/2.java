198f9469f47f1d48a283502477e139dd2227be96
TAZDIK HOSSAIN <tazdik755@gmail.com>
2024-04-08 15:16:38 +0600

    Add files via upload

---
+++ JAVA-Advanced/2.java
@@ -0,0 +1,78 @@
+class two {
+
+    // if else
+    public static void main(String[] args)
+    {
+        // int x = 15;
+        // if(x<10 && x<=20)
+        //     System.out.println("Right");
+        // else
+        //     System.out.println("False");
+
+
+        // int a = 8;
+        // int y = 9;
+        // if(x>y)
+        // {
+        //     System.out.println(x);
+        //     System.out.println("Yes");
+        // }
+        // else    
+        //     System.out.println(y);
+
+
+        // While loop
+
+        // int i = 1;
+        // while(i<=4)
+        // {
+        // System.out.println("HEllo" + i);
+        // i++;
+        // }
+
+
+        
+        // int i = 1;
+        // while(i<=4)
+        // {
+        //     System.out.println("wow" +i);
+        //     int j= 1;
+        //     while(j<=3){
+        //         System.out.println("HEllo" + j);
+        //         j++;
+        //     }
+        //     i++;
+        // }
+        // System.out.println("Do" +i);
+
+
+
+
+        // Do While loop
+        // int h = 5;
+        // do{
+        //     System.out.println("Hero" + h);
+        //     h++;
+        // }
+        // while(h<=4);
+
+
+        //For loop
+        // for(int k=1; k<=4; k++)
+        // {
+        //     System.out.println("Jello" + k);
+        // }
+
+        for (int s = 1; s<=7; s++)
+        {
+            System.out.println("Day "+s);
+
+            for(int j=1; j<=9; j++)
+            {
+                System.out.println(" "+(j+8) + "-"+(j+9));
+            }
+        }
+        
+    }
+    
+}
---
+++ JAVA-Advanced/Hello.java
@@ -0,0 +1,83 @@
+class Hello
+{
+    public static void main(String a[])
+        { 
+            // 1st code
+            System.out.print("Hello World");
+            System.out.println(3+3);
+
+            // Variable----
+            int num1 = 3;
+            System.out.println(num1);
+            int num2 = 4;
+            System.out.println(num2);
+
+            int num3 = 10;
+            int num4 = 25;
+            int result  = num3 + num4;
+            System.out.println(result);
+
+            // Data type-------
+            byte by= 127;
+            short sh = 554;
+            long l = 456l;
+            float f= 4.5f;
+            double d  = 4.4;
+            char c= 'd';
+            boolean b = true;
+
+            // Literals------
+            int num5 = 0b101;
+            System.out.println(num5);
+            int num6 = 0x7E;
+            System.out.println(num6);
+            double num7 = 12e10;
+            System.out.println(num7);
+
+            char s = 'a';
+            s++;
+            System.out.println(s);
+
+
+            // Type conversion -- Type casting
+            // byte by = 127;
+            // int ai = 127;
+            // by = ai;
+            // ai = by;
+            // by = (byte)ai;
+
+            int e = 257;
+            byte k = (byte) e;
+            System.out.println(k);
+
+            // Type emotion\
+            byte aw = 10;
+            byte bw = 30;
+            int res= aw*bw;
+            System.out.println();
+
+            // Assingment operator
+            int num8=3;
+            int num9 = 3;
+            int result2 = num8+num9;
+
+            num8 +=2;
+            System.out.println(num8);
+
+            num8++;  // post increment
+            ++num8;  // pre increment
+
+            int result3 = num8++;
+            int result4 = ++num8;
+
+            
+
+
+
+
+
+
+
+       }
+
+}
