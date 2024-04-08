15d53b01cb6becd798b3394de44179de6b1669ab
TAZDIK HOSSAIN <tazdik755@gmail.com>
2024-04-08 17:36:43 +0600

    Add files via upload

---
+++ JAVA-Advanced/first/src/main/java/com/java/first/FirstController.java
@@ -0,0 +1,14 @@
+package com.java.first;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+    
+    @RequestMapping("/hello")
+    public String hello()
+    {
+        return "Hello Tazdik ";
+    }
+}
